3cd8bf12f2fd2483b4541ef43642a75babec20a2
A <tanaybhattacharjee@Tanays-MacBook-Air.local>
2018-04-15 15:59:43 +0530

    Initial Commit

---
+++ tableviewsection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableviewsection
+//
+//  Created by Tanay Bhattacharjee on 15/04/18.
+//  Copyright © 2018 Tanay Bhattacharjee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
